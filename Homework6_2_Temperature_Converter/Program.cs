c92d6e7c725265bbc3607f2dd734d01e843d78d3
Lingfey <bokanchalangman@gmail.com>
2022-09-29 04:04:12 +0800

    Temperature Converter
    Custom Class, custom methods, Regex, switch, do-while loop

---
+++ Homework6_2_Temperature_Converter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace Homework6_2_Temperature_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("E.g.: 40C to F");
+                string request = Console.ReadLine();
+                string[] splitRequest = Regex.Split(request, @"([\d.-]+) ?(\w+?) to (\w+?)");
+                double initialTemperature = Convert.ToDouble(splitRequest[1]);
+                string initialUnit = splitRequest[2];
+                string convertedUnit = splitRequest[3];
+                switch (convertedUnit)
+                {
+                    case "k":
+                    case "K":
+                    case "kelvin":
+                    case "Kelvin":
+                        Console.WriteLine($"{initialTemperature}{initialUnit}" +
+                            $" is {TempConverter.ToKelvin(initialTemperature, initialUnit):0.0}" +
+                            $"{convertedUnit}");
+                        break;
+                    case "C":
+                    case "c":
+                    case "celsius":
+                    case "Celsius":
+                        Console.WriteLine($"{initialTemperature}{initialUnit}" +
+                            $" is {TempConverter.ToCelsius(initialTemperature, initialUnit):0.0}" +
+                            $"{convertedUnit}");
+                        break;
+                    case "F":
+                    case "f":
+                    case "fahrenheit":
+                    case "Fahrenheit":
+                        Console.WriteLine($"{initialTemperature}{initialUnit}" +
+                            $" is {TempConverter.ToFahrenheit(initialTemperature, initialUnit):0.0}" +
+                            $"{convertedUnit}");
+                        break;
+                    default:
+                        Console.WriteLine("Wrong format");
+                        break;
+                }
+                Console.WriteLine("Try again?(y/n)");
+                answer = Console.ReadLine();
+            }
+            while (answer == "y" || answer == "yes");
+          
+        }
+    }
+}
---
+++ Homework6_2_Temperature_Converter/TempConverter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Homework6_2_Temperature_Converter
+{
+    public class TempConverter
+    {
+        public static double? ToCelsius(double temperature, string initialUnit)
+        {
+            switch (initialUnit)
+            {
+                case "F":
+                case "f":
+                case "fahrenheit":
+                case "Fahrenheit":
+                    return (temperature - 32) * 0.5556;
+                case "K":
+                case "k":
+                case "kelvin":
+                case "Kelvin":
+                    return temperature - 273.15;
+                default:
+                    return null;
+            }
+        }
+        public static double? ToFahrenheit(double temperature, string initialUnit)
+        {
+            switch (initialUnit)
+            {
+                case "C":
+                case "c":
+                case "celsius":
+                case "Celsius":
+                    return temperature * 1.8 + 32;
+                case "K":
+                case "k":
+                case "kelvin":
+                case "Kelvin":
+                    return (temperature - 273.15) * 1.8 + 32;
+                default:
+                    return null;
+            }
+        }
+        public static double? ToKelvin(double temperature, string initialUnit)
+        {
+            switch (initialUnit)
+            {
+                case "C":
+                case "c":
+                case "celsius":
+                case "Celsius":
+                    return temperature + 273.15;
+                case "F":
+                case "f":
+                case "fahrenheit":
+                case "Fahrenheit":
+                    return (temperature - 32) * 0.5556 + 273.15;
+                default:
+                    return null;
+            }
+        }
+    }
+}
+
